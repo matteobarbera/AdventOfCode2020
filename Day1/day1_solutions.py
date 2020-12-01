9f9993f48673eef91fade8361cb5b7e9e3611cfe
matteobarbera <matteo.barbera97@gmail.com>
2020-12-01 11:22:57 +0100

    No reason to read file twice

--- Day1/day1_solutions.py
+++ Day1/day1_solutions.py
@@ -3,25 +3,25 @@ from math import prod
 
 
 def read_input():
-    entries = []
+    lst = []
     with open("day1_input.txt")as f:
         for line in f:
-            entries.append(int(line))
-    return entries
+            lst.append(int(line))
+    return lst
 
 
-def sum_to_2020(n_entries: int):
-    entries = read_input()
-    for p in permutations(entries, n_entries):
+def sum_to_2020(lst: list, n: int):
+    for p in permutations(lst, n):
         if sum(p) == 2020:
             print(tuple(p))
             return prod(p)
 
 
 if __name__ == "__main__":
+    entries = read_input()
     print("="*10 + " Part 1 " + "="*10)
-    day1_part1_solution = sum_to_2020(2)
+    day1_part1_solution = sum_to_2020(entries, 2)
     print(f"Product of 2020 sum: {day1_part1_solution}")
     print("=" * 10 + " Part 2 " + "=" * 10)
-    day1_part2_solution = sum_to_2020(3)
+    day1_part2_solution = sum_to_2020(entries, 3)
     print(f"Product of 2020 sum: {day1_part2_solution}")
